d190699d58a52da6bd3ed441b5d7c8e8c476ce91
carmenllenado <39427352+carmenllenado@users.noreply.github.com>
2023-08-17 11:25:48 +0800

    added weather API to js

--- index.js
+++ index.js
@@ -27,11 +27,33 @@ let getDate = document.querySelector("#date-time");
 getDate.innerHTML = `${currentDay}, ${fullTime}`;
 
 //Feature 2: Search engine [Now with weather API]
-function updateCity(event) {
+function cityResult(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#searchbar");
   let display = document.querySelector("#display");
   display.innerHTML = `${cityInput.value}`;
+
+  let cityName = `${cityInput.value}`;
+  let apiKey = `535cacbb3f8a0df0aeb4790235b9541f`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?`;
+
+  function updateTemp(response) {
+    let tempDisplay = document.querySelector("#temp-display");
+    tempDisplay.innerHTML = `${Math.round(response.data.main.temp)}ºC`;
+
+    let prediction = document.querySelector("#prediction");
+    prediction.innerHTML = `${response.data.weather[0].description}`;
+
+    let humidity = document.querySelector("#humidity");
+    humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+
+    let wind = document.querySelector("#wind");
+    wind.innerHTML = `Wind: ${response.data.wind.speed} km/h`;
+  }
+  axios
+    .get(`${apiUrl}q=${cityName}&appid=${apiKey}&units=metric`)
+    .then(updateTemp);
 }
+
 let searchbar = document.querySelector("#city-search");
-searchbar.addEventListener("submit", updateCity);
+searchbar.addEventListener("submit", cityResult);
